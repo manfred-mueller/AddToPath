42fa06acdce7189882ca154770ef137cc09023a1
Manfred Mueller <mail@manfred-mueller.org>
2023-08-08 07:58:10 +0200

    Update AddToPath.cs

--- AddToPath/AddToPath.cs
+++ AddToPath/AddToPath.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
@@ -13,24 +13,31 @@ namespace AddToPath
     [COMServerAssociation(AssociationType.Directory)]
     public class AddToPath : SharpContextMenu
     {
+        // Get the path of the selected item (first selected path).
         private string SelectedItemPath => SelectedItemPaths.First();
+
+        // Construct a regular expression pattern to match the selected item in the PATH.
         private string ItemRegexPattern =>  $"(?<=^|;){Regex.Escape(SelectedItemPath)}(?=$|;)";
 
+        // Override method to determine whether to show the context menu.
         protected override bool CanShowMenu()
         {
-            return true;
+            return true; // Always show the menu.
         }
 
+        // Override method to create the context menu.
         protected override ContextMenuStrip CreateMenu()
         {
             var menu = new ContextMenuStrip();
             var isInPath = IsInPath();
 
+            // Create a menu item based on whether the selected item is in the PATH.
             var itemCountLines = new ToolStripMenuItem()
             {
                 Text = isInPath ? "Remove from PATH (User)" : "Add to PATH (User)",
             };
 
+            // Handle the click event for the menu item.
             itemCountLines.Click += (sender, args) =>
             {
                 if (isInPath)
@@ -43,17 +50,21 @@ namespace AddToPath
                 }
             };
 
+            // Add the menu item to the context menu.
             menu.Items.Add(itemCountLines);
 
             return menu;
         }
 
+        // Run the specified method in a separate thread.
         private void RunInSeparateThread(ThreadStart start)
         {
             var worker = new Thread(start);
             worker.SetApartmentState(ApartmentState.STA);
             worker.Start();
         }
+
+        // Add the selected item to the PATH.
         private void AddSelectedItemToPath()
         {
             var oldPath = GetPath();
@@ -61,6 +72,7 @@ namespace AddToPath
             SetPath(newPath);
         }
         
+        // Remove the selected item from the PATH.
         private void RemoveSelectedItemFromPath()
         {
             var oldPath = GetPath();
@@ -68,16 +80,19 @@ namespace AddToPath
             SetPath(newPath);
         }
 
+        // Check if the selected item is already in the PATH.
         private bool IsInPath()
         {
             return Regex.Match(GetPath(), ItemRegexPattern).Success;
         }
         
+        // Get the current PATH environment variable.
         private static string GetPath()
         {
             return Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.User);
         }
 
+        // Set the PATH environment variable to the specified new path.
         private static void SetPath(string newPath)
         {
             Environment.SetEnvironmentVariable("Path", newPath, EnvironmentVariableTarget.User);
